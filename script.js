ac342f21d9a39a39050e0715ea08de699b78e229
Hunny-Saini <hunny.ux@gmail.com>
2023-05-07 23:25:49 +0530

    djdn

---
+++ script.js
@@ -0,0 +1,19 @@
+// myProducts.filter((item)=>item.title.includes(search.value))
+
+// myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
+
+//if user is alredy logged in
+if(localStorage.getItem("currUser")){
+    window.location.href = "../shop/index.html";
+}
+
+const loginBtn = document.getElementById("login-btn");
+const signupBtn = document.getElementById("signup-btn");
+
+loginBtn.addEventListener("click", ()=>{
+    window.location.href = "./login/index.html";
+});
+
+signupBtn.addEventListener("click",()=>{
+    window.location.href ="./signup/index.html"
+});
